5ee41fdb3c2ba5b8fb6da74010cd10b83eb811fe
AsanovAlbek <87094098+AsanovAlbek@users.noreply.github.com>
2024-06-06 17:16:50 +0300

    Update router.py

--- api/router.py
+++ api/router.py
@@ -10,6 +10,10 @@ class FileRequestBody(BaseModel):
 
 routers = APIRouter()
 
+@routers.get('/', tags=['start'])
+async def start():
+    return {'message', 'successfuly connected'}
+
 @routers.post('/', tags=['word'])
 async def add_word(word: wordDto.Word, db: Session = Depends(get_connection)):
     return service.add_word(data = word, db = db)
